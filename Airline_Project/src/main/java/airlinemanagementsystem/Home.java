a6ac9393fe3f6d8c766b2b5453f4264de6cdcedd
khalid <mohdkhalidit56@gmail.com>
2024-11-12 11:41:45 +0530

    Initial Commit

---
+++ Airline_Project/src/main/java/airlinemanagementsystem/Home.java
@@ -0,0 +1,103 @@
+package airlinemanagementsystem;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import com.Project1.Airline_Project.AddCustomer;
+import com.Project1.Airline_Project.BoardingPass;
+import com.Project1.Airline_Project.BookFlight;
+import com.Project1.Airline_Project.Cancel;
+import com.Project1.Airline_Project .FlightInfo;
+import com.Project1.Airline_Project.JourneyDetails;
+public class Home extends JFrame implements ActionListener {
+	
+
+
+	public Home(){
+		
+		setLayout(null);
+        
+		ImageIcon i1 = new ImageIcon(getClass().getClassLoader().getResource("com/Project1/Airline_Project/icons/front.jpg"));
+		 JLabel image = new JLabel(i1);
+	        image.setBounds(0, 0, 1600, 800);
+	        add(image);
+		
+	        JLabel heading = new JLabel("AIR INDIA WELCOMES YOU");
+	        heading.setBounds(500, 40, 1000, 40);
+	        heading.setForeground(Color.BLUE);
+	        heading.setFont(new Font("Tahoma", Font.PLAIN, 36));
+	        image.add(heading);
+	        
+	        JMenuBar menubar = new JMenuBar();
+	        setJMenuBar(menubar);
+
+	        JMenu details = new JMenu("Details");
+	        menubar.add(details);
+	        
+	        
+	        
+	        JMenuItem flightDetails = new JMenuItem("Flight Details");
+	        flightDetails.addActionListener(this);
+	        details.add(flightDetails);
+	        
+	        JMenuItem customerDetails = new JMenuItem("Add Customer Details");
+	        customerDetails.addActionListener(this);
+	        details.add(customerDetails);
+	        
+	        JMenuItem bookFlight = new JMenuItem("Book Flight");
+	        bookFlight.addActionListener(this);
+	        details.add(bookFlight);
+	        
+	        JMenuItem journeyDetails = new JMenuItem("Journey Details");
+	        journeyDetails.addActionListener(this);
+	        details.add(journeyDetails);
+	        
+	        JMenuItem ticketCancellation = new JMenuItem("Cancel Ticket");
+	        ticketCancellation.addActionListener(this);
+	        details.add(ticketCancellation);
+	        
+	        JMenu ticket = new JMenu("Ticket");
+	        menubar.add(ticket);
+	        
+	        JMenuItem boardingPass = new JMenuItem("Boarding Pass");
+	        ticket.add(boardingPass);
+
+	        boardingPass.addActionListener(this);
+	        
+	        
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setVisible(true);
+		
+	}
+	public void actionPerformed(ActionEvent ae) {
+		String text = ae.getActionCommand();
+		
+		 if (text.equals("Add Customer Details")) {
+		        new AddCustomer();
+		    } else if (text.equals("Flight Details")) {
+		        new FlightInfo();
+		    } else if (text.equals("Book Flight")) {
+		        new BookFlight();
+		    } else if (text.equals("Journey Details")) {
+		        new JourneyDetails();
+		    }  else if (text.equals("Cancel Ticket")){
+		    	new Cancel();
+		    }
+		    else if(text.equals("Boarding Pass")){
+		    	new BoardingPass();
+		    }
+		}
+	
+	public static void main(String[] args) {
+		new Home();
+	}
+}
